c60a7b5d25727c9935458e1bc6707585484ae01f
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-03-10 20:15:49 +0200

    terraform-config-cluster

--- main.tf
+++ main.tf
@@ -1,46 +1,58 @@
-#In Azure, all infrastructure elements such as virtual machines, storage, and our Kubernetes cluster need to be attached to a resource group.
-resource "azurerm_resource_group" "aks-rg" {
-  name     = var.resource_group_name
-  location = var.location
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.95.0"
+    }
+  }
 }
 
-# resource "azurerm_role_assignment" "role_acrpull" {
-#   scope                            = azurerm_container_registry.acr.id
-#   role_definition_name             = "AcrPull"
-#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-#   skip_service_principal_aad_check = true
-# }
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 
-# resource "azurerm_container_registry" "acr" {
-#   name                = var.acr_name
-#   resource_group_name = azurerm_resource_group.aks-rg.name
-#   location            = var.location
-#   sku                 = "Standard"
-#   admin_enabled       = false
-# }
+# Create a resource group
+resource "azurerm_resource_group" "product" {
+  name     = "product-resources"
+  location = "East US"
+}
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
-  location            = var.location
-  resource_group_name = azurerm_resource_group.aks-rg.name
-  dns_prefix          = var.cluster_name
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "product" {
+  name                = "product-network"
+  resource_group_name = azurerm_resource_group.product.name
+  location            = azurerm_resource_group.product.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_kubernetes_cluster" "product" {
+  name                = "product-aks1"
+  location            = azurerm_resource_group.product.location
+  resource_group_name = azurerm_resource_group.product.name
+  dns_prefix          = "productaks1"
 
   default_node_pool {
-    name                = "system"
-    node_count          = var.system_node_count
-    vm_size             = "Standard_DS2_v2"
-    type                = "VirtualMachineScaleSets"
-    # availability_zones  = [1, 2, 3]
-    enable_auto_scaling = false
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  network_profile {
-    load_balancer_sku = "Standard"
-    network_plugin    = "kubenet"
+  tags = {
+    Environment = "Production"
   }
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.product.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.product.kube_config_raw
+
+  sensitive = true
 }
