7bf659bdebc9506966e279e70a6836b981ef0116
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-03-17 16:55:25 +0200

    deploy-test

--- main.tf
+++ main.tf
@@ -47,10 +47,6 @@ resource "azurerm_kubernetes_cluster" "product" {
   }
 }
 
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.product.kube_config.0.client_certificate
-}
-
 output "kube_config" {
   value = azurerm_kubernetes_cluster.product.kube_config_raw
 
