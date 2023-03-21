95e114de898e8d349c2f0c3cd372fbbbe71fe8fd
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-03-22 01:07:18 +0200

    added_lb_sg_test

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -14,8 +14,50 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "product" {
-  name     = "product-resources"
+  name     = "product"
   location = "East US"
+}
+
+resource "azurerm_network_security_group" "product" {
+  name                = "product-security-group"
+  location            = azurerm_resource_group.product.location
+  resource_group_name = azurerm_resource_group.product.name
+
+  security_rule {
+    name                       = "azure-team"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Create public ip
+resource "azurerm_public_ip" "product" {
+  name                = "PublicIPForLB"
+  location            = azurerm_resource_group.product.location
+  resource_group_name = azurerm_resource_group.product.name
+  allocation_method   = "Static"
+}
+
+# Create load balancer
+resource "azurerm_lb" "product" {
+  name                = "LoadBalancer"
+  location            = azurerm_resource_group.product.location
+  resource_group_name = azurerm_resource_group.product.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.product.id
+  }
 }
 
 # Create a virtual network within the resource group
