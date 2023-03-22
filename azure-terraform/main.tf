55e8e0cb6e1371925a1747f27a035f6cc71f2b35
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-03-23 00:52:53 +0200

    updates_added_providers

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.47.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "product" {
   name     = "product"
