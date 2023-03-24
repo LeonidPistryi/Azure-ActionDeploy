76c39495e2834d9f843909bc66e07b41fb1499dc
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-03-24 12:55:56 +0200

    updates

--- azure-terraform/providers.tf
+++ azure-terraform/providers.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "tfstate"
-    storage_account_name = "tfstateitsprout"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name  = "microfostsitnamedtest"
+    storage_account_name = "storage123678test"
+    container_name       = "containername"
+    key                  = "terraform.containername"
   }
 }
 
