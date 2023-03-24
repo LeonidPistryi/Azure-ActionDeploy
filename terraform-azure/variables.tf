2ab97b7c86efe002dec4f498674b7831f8b29ad4
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-03-24 14:00:56 +0200

    updates

---
+++ terraform-azure/variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "agent_count" {
+  default = 1
+}
+
+# variable "ssh_public_key" {
+#   default = "~/.ssh/id_rsa.pub"
+# }
