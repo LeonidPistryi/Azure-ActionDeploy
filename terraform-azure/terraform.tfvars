73718e973f4fae9e233c7f8d3804377a9553e834
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-03-27 13:09:36 +0300

    deploy-test

--- terraform-azure/terraform.tfvars
+++ terraform-azure/terraform.tfvars
@@ -1,4 +1,4 @@
-location = "eastus"
 cluster_name = "product"
 kubernetes_version = "1.24.9"
 system_node_count = 1
+resource_group_location = "useast"
--- terraform-azure/variables.tf
+++ terraform-azure/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_location" {
-  default     = "eastus"
+  type = string
   description = "Location of the resource group."
 }
 
