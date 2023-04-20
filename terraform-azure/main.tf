fc55a31d70f879da91ce2279da4dfeac8eac1901
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-04-20 15:43:22 +0300

    test

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -110,7 +110,7 @@ resource "azurerm_subnet" "product" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.product.name
   virtual_network_name = azurerm_virtual_network.product.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["0.0.0.0/0"]
 }
 
 # Create fireawall
