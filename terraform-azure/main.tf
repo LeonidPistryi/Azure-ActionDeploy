e60a1f794fc3ab857cf90d08d455dd66885595fc
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-04-21 15:04:50 +0300

    start

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -84,7 +84,6 @@ resource "local_file" "kubeconfig" {
   content  = azurerm_kubernetes_cluster.product.kube_config_raw
 }
 
-
 #Create mysql a server
 resource "azurerm_mysql_server" "product" {
   name                = "wpteam-mysqlserver"
