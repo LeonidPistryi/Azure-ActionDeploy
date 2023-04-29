f63721cf684692df4d35e36dd392fe556f941d23
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-04-29 18:38:30 +0300

    start

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -4,58 +4,6 @@ resource "azurerm_resource_group" "product" {
   location = var.resource_group_location
 }
 
-# Create sg
-# resource "azurerm_network_security_group" "product" {
-#   name                = "product-security-group"
-#   location            = azurerm_resource_group.product.location
-#   resource_group_name = azurerm_resource_group.product.name
-
-#   security_rule {
-#     name                       = "azure-team"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "*"
-#     source_address_prefix      = "0.0.0.0/0"
-#     destination_address_prefix = "*"
-#   }
-
-#   tags = {
-#     environment = "dev"
-#   }
-# }
-
-# Create public ip
-# resource "azurerm_public_ip" "product" {
-#   name                = "PublicIPForLB"
-#   location            = azurerm_resource_group.product.location
-#   resource_group_name = azurerm_resource_group.product.name
-#   allocation_method   = "Static"
-#   sku                 = "Standard"
-# }
-
-# Create load balancer
-# resource "azurerm_lb" "product" {
-#   name                = "LoadBalancer"
-#   location            = azurerm_resource_group.product.location
-#   resource_group_name = azurerm_resource_group.product.name
-
-#   frontend_ip_configuration {
-#     name                 = "PublicIPAddress"
-#     public_ip_address_id = azurerm_public_ip.product.id
-#   }
-# }
-
-# Create a virtual network within the resource group
-# resource "azurerm_virtual_network" "product" {
-#   name                = "product-network"
-#   resource_group_name = azurerm_resource_group.product.name
-#   location            = azurerm_resource_group.product.location
-#   address_space       = ["10.0.0.0/16"]
-# }
-
 # Create k8s
 resource "azurerm_kubernetes_cluster" "product" {
   name                = var.cluster_name
