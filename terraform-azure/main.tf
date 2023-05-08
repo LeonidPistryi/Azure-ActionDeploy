b001e6c2c4db3cc41f62e9a4c070cf1c4584cbee
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-05-08 23:01:32 +0300

    destroy

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -32,6 +32,7 @@ resource "local_file" "kubeconfig" {
   content  = azurerm_kubernetes_cluster.product.kube_config_raw
 }
 
+
 #Create mysql a server
 resource "azurerm_mysql_server" "product" {
   name                = "wpteam-mysqlserver"
